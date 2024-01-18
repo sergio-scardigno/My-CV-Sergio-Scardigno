f73e6cc14f07a7a68811cfe504b0c1c71aeeba3f
Sergio <sergioscardigno82@gmail.com>
2024-01-18 11:31:04 -0300

    first commit

---
+++ src/data/resume-data.tsx
@@ -0,0 +1,437 @@
+import {
+  AmbitLogo,
+  BarepapersLogo,
+  BimLogo,
+  CDGOLogo,
+  ClevertechLogo,
+  ConsultlyLogo,
+  EvercastLogo,
+  Howdy,
+  JarockiMeLogo,
+  JojoMobileLogo,
+  Minimal,
+  MobileVikingsLogo,
+  MonitoLogo,
+  NSNLogo,
+  ParabolLogo,
+  TastyCloudLogo,
+  YearProgressLogo,
+} from "@/images/logos";
+import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
+
+export const RESUME_DATA = {
+  name: "Ian Hobbs",
+  initials: "IJ",
+  location: "Sydney, Australia, EST",
+  locationLink: "https://www.google.com/maps/place/Sydney",
+  about:
+    "Creative with a focus on Web, Photomedia and Coding",
+  summary:
+    "Thanks to everybody who helped. Gratefull for everyone's support.",
+
+  avatarUrl: "https://avatars.githubusercontent.com/ianhobbs",
+  personalWebsiteUrl: "https://ianhobbsmedia.com.au",
+  contact: {
+    email: "admin@ianhobbsmedia.com.au",
+    tel: "+61411032601",
+    social: [
+      {
+        name: "GitHub",
+        url: "https://github.com/ianhobbs",
+        icon: GitHubIcon,
+      },
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/ianhobbs/",
+        icon: LinkedInIcon,
+      },
+      {
+        name: "X",
+        url: "https://twitter.com/ianHobbs_media",
+        icon: XIcon,
+      },
+    ],
+  },
+  education: [
+    {
+      school: "University of Sydney, Sydney College of Art",
+      degree: "BA(Hons) Major in Photomedia / Electronic Media",
+      start: "1997",
+      end: "1998",
+    },
+    {
+      school: "University of NSW",
+      degree: "Bachelor's Degree in Fine Art, Major in Photomedia",
+      start: "1980",
+      end: "1984",
+    },
+  ],
+  work: [
+    {
+      company: "Ian Hobbs Media",
+      link: "https://ianhobbsmedia.com.au",
+      badges: [],
+      title: "Owner",
+      logo: ParabolLogo,
+      start: "2020",
+      end: "now",
+      description:
+        "Web production. Experience includes; Videographer, Editor, Photographer,Front-end development, Animator, Storyboarding, Prototype development,Design, Server management and Data management. Client and project management. Art direction and concept development. Media strategy and Editorial strategy.",
+    },
+    {
+      company: "Codey",
+      link: "https://codey.com.au",
+      badges: [],
+      title: "Owner",
+      logo: ClevertechLogo,
+      start: "2011",
+      end: "now",
+      description:
+        "Hosting server and site maintenance. Server management and Data management. Client service. Backup management. Site optimisation. Threat mitigation",
+    },
+    {
+      company: "Sydney University SCA",
+      link: "https://www.sydney.edu.au/arts/schools/sydney-college-of-the-arts.html",
+      badges: ["Contract"],
+      title: "Research Officer",
+      logo: JojoMobileLogo,
+      start: "10/2018",
+      end: "12/2018",
+      description:
+        "Contract to research, collect and promote staff research outputs for University pages. Videography, Photography and research development. Prototyping web pages",
+    },
+    {
+      company: "Sydney University SCA",
+      link: "https://www.sydney.edu.au/arts/schools/sydney-college-of-the-arts.html",
+      badges: ["Sessional"],
+      title: "Lecturer",
+      logo: NSNLogo,
+      start: "2010",
+      end: "2014",
+      description: "Design and deliver subjects in Photography and Electronic & New Media.",
+    },
+    {
+      company: "Insearch UTS",
+      link: "https://utscollege.edu.au",
+      badges: ["Sessional"],
+      title: "Lecturer",
+      logo: NSNLogo,
+      start: "2010",
+      end: "2014",
+      description: "Deliver subjects in Digital Literacy",
+    },
+    {
+      company: "SAE + Quamtum",
+      link: "https://sae.edu.au/",
+      badges: ["Sessional"],
+      title: "Lecturer",
+      logo: NSNLogo,
+      start: "2007",
+      end: "2010",
+      description: "Deliver courses in Web, Design, Video and Animation. Audio",
+    },
+    {
+      company: "University of Newcastle. Fine Arts Faculty",
+      link: "https://www.newcastle.edu.au",
+      badges: ["Sessional"],
+      title: "Lecturer",
+      logo: NSNLogo,
+      start: "2001",
+      end: "2006",
+      description: "Design and deliver courses in Digital Imaging, Video",
+    },
+    {
+      company: "University of Western Sydney. Communications & Design.",
+      link: "https://www.westernsydney.edu.au",
+      badges: ["Sessional"],
+      title: "Lecturer",
+      logo: NSNLogo,
+      start: "2001",
+      end: "2006",
+      description: "Design and New Media, Web and Time based media.",
+    },
+    {
+      company: "Enmore TAFE",
+      link: "https://www.tafensw.edu.au/design-centre-enmore",
+      badges: ["Sessional"],
+      title: "Teacher",
+      logo: NSNLogo,
+      start: "2001",
+      end: "2006",
+      description: "Design and deliver courses in New media, Flash, Web Photo-media, Design. Animation",
+    }
+  ],
+
+
+  awards: [
+    {
+      award: "Recipient of Premiers Department Grant with Penrith Regional Gallery",
+      year: "2003",
+    },
+    {
+      award: "Recipient of New Media Grant for Interactive Works, Australia Council.",
+      year: "1999",
+    },
+    {
+      award: "The Art Directors Club, New York, NY 75th Awards, 'Merit Award for Editorial Publications' for Object Magazine, Craft Council of NSW.",
+      year: "1996",
+    },
+    {
+      award: "National Gallery of Australia Collection. 'Sea Shell Headset' Joyce Hinterding & Ian Hobbs",
+      year: "1988",
+    },
+    {
+      award: "National Gallery of Australia Collection. Poster, 'May 1st Club - Smash Capital'.",
+      year: "1983",
+    },
+
+
+  ],
+
+  published: [
+    {
+      title: "Chester Nealie Etched in Fire",
+      pub: "Ron Sang Publications NZ",
+      year: "2016",
+    },
+    {
+      title: "Robyn Backen: Backspace",
+      pub: "Boccolate Publishing AU",
+      year: "2011",
+    },
+    {
+      title: "Liz Williamson: Textiles",
+      pub: "Craft Council of NSW AU",
+      year: "2009",
+    },
+    {
+      title: "For Mathew and Others: Journey with Schizophrenia",
+      pub: "The University of New South Wales AU",
+      year: "2006",
+    },
+    {
+      title: "Degree Show 1998 SCA The University of Sydney: Catalogue",
+      pub: "Sydney College of The Arts AU",
+      year: "1998",
+    },
+  ],
+
+
+  // end insert
+  
+  skills: [
+    "Photography",
+    "Studio Photography",
+    "PHP-CMS",
+    "JavaScript",
+    "CSS",
+    "Tailwind",
+    "Project Management",
+    "Web Production",
+    "Animation",
+    "After Effects",
+    "Audio production"
+  ],
+  projects: [
+    {
+      title: "Clear Control Website",
+      techStack: [
+        "KirbyCMS",
+        "Tailwind",
+        "AlpineJS",
+        "GSAP",
+        "Design",
+        "Content",
+        "Full Stack Developer",
+      ],
+      description: "Created a business website with seductive animations and strong case study representations",
+      logo: ConsultlyLogo,
+      link: {
+        label: "clearcontrol.com.au",
+        href: "https://www.clearcontrol.com.au/",
+      },
+    },
+    {
+      title: "Cementa Website",
+      techStack: ["PerchCMS", "AlpineJS", "Blythe", "Content",""],
+      description:
+        "A festival site showcasing artists and Festival events",
+      logo: MonitoLogo,
+      link: {
+        label: "cementa.com.au",
+        href: "https://cementa.com.au/",
+      },
+    },
+    {
+      title: "A Thousand Words Website",
+      techStack: ["PerchCMS", "AlpineJS", "Animation"],
+      description:
+        "A website of the Museum of Sydney 'A Thousand Words' exhibition",
+      logo: JarockiMeLogo,
+      link: {
+        label: "atwonline.com.au",
+        href: "https://atwonline.com.au",
+      },
+    },
+    {
+      title: "Uno",
+      techStack: ["After Effects","Video"],
+      description:
+        "Develop animations for custom Architectural LED screens for Robyn Backen",
+      logo: Minimal,
+      link: {
+        label: "ianhobbsmedia.com.au/uno",
+        href: "https://ianhobbsmedia.com.au/uno",
+      },
+    },
+    {
+      title: "Thought Train Animation",
+      techStack: ["After Effects","Video"],
+      description:
+        "Animation for Wynscreen Video project with Robyn Backen",
+      logo: BarepapersLogo,
+      link: {
+        label: "ianhobbsmedia.com.au/thought-train",
+        href: "https://ianhobbsmedia.com.au/thought-train",
+      },
+    },
+    {
+      title: "Art Photography",
+      techStack: ["Art Reproduction"],
+      description: "Fine Art documentation projects",
+      logo: YearProgressLogo,
+      link: {
+        label: "ianhobbsmedia.com.au/art",
+        href: "https://ianhobbsmedia.com.au/art",
+      },
+    },
+    {
+      title: "Australian Ceramics Website",
+      techStack: [
+        "Full Stack Developer",
+        "PerchCMS",
+        "AlpineJS",
+        "HTMLX",
+      ],
+      description:
+        "An e-commerce site for Australian Ceramics Organisation",
+      logo: ParabolLogo,
+      link: {
+        label: "australianceramics.com",
+        href: "https://australianceramics.com",
+      },
+    },
+    {
+      title: "Rosie Boylan Website",
+      techStack: [
+        "Full Stack Developer",
+        "Wordpress",
+        "Shopify",
+        "liquid",
+        "Product Manager"
+      ],
+      description:
+        "An e-commerce site and shopify store for Rosie Boylan Headwear",
+      logo: EvercastLogo,
+      link: {
+        label: "rosieboylan.com",
+        href: "https://rosieboylan.com",
+      },
+    },
+    {
+      title: "Sue Pedley Site",
+      techStack: ["KirbyCMS", "Front End Dev", "AlpineJS"],
+      description:
+        "Portfolio site for Sue Pedley",
+      logo: MobileVikingsLogo,
+      link: {
+        label: "suepedley.com.au",
+        href: "https://suepedley.com.au/",
+      },
+    },
+    {
+      title: "Turpin Crawford Studios Site",
+      techStack: ["Web development", "Photography", "Video","PerchCMS"],
+      description:
+        "Portfolio web development for some lovely artists. Lots of Video and photo.",
+      logo: Howdy,
+      link: {
+        label: "turpincrawfordstudio.com.au/",
+        href: "https://turpincrawfordstudio.com.au/",
+      },
+    },
+    {
+      title: "Skin 8",
+      techStack: ["Web Development", "Static HTML","Pug","Sass"],
+      description:
+        "A nice high end fashion brand site by Lucy Walker",
+      logo: TastyCloudLogo,
+      link: {
+        label: "ianhobbsmedia.com.au/skin8",
+        href: "https://ianhobbsmedia.com.au/skin8",
+      },
+    },
+    {
+      title: "A Thousand Words Video",
+      techStack: ["After Effects"],
+      description:
+        "Exibiton animation of word clouds",
+      logo: AmbitLogo,
+      link: {
+        label: "ianhobbsmedia.com.au/a-thousand-words",
+        href: "https://ianhobbsmedia.com.au/a-thousand-words",
+      },
+    },
+    {
+      title: "Nano-black",
+      techStack: [
+        "Browser Plugin",
+        "HTML hack",
+      ],
+      description:
+        "Makes every picture black",
+      logo: EvercastLogo,
+      link: {
+        label: "github.com/ianhobbs/nano_black",
+        href: "https://github.com/ianhobbs/nano_black",
+      },
+    },
+    {
+      title: "Rejmyre Art Lab Site",
+      techStack: ["Web Development","PerchCMS"],
+      description:
+        "A Swedish art org's home site wonderful research.",
+      logo: BimLogo,
+      
+      link: {
+        label: "rejmyreartlab.org/",
+        href: "https://www.rejmyreartlab.org/",
+      },
+    },
+    {
+      title: "Architectural Photography",
+      techStack: ["Photograpy"],
+      description:
+        "Architectural work",
+      logo: BimLogo,
+      
+      link: {
+        label: "ianhobbsmedia.com.au/architecture",
+        href: "https://ianhobbsmedia.com.au/architecture",
+      },
+    },
+
+    {
+      title: "Video Animation",
+      techStack: ["Video"],
+      description:
+        "Video Work",
+      logo: BimLogo,
+      
+      link: {
+        label: "ianhobbsmedia.com.au/video-animation",
+        href: "https://ianhobbsmedia.com.au/video-animation",
+      },
+    },
+  ],
+} as const;
