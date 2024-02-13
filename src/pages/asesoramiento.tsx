b690338ba972925f887464fdd88805534ceb1109
sergio <sergioscardigno82@gmail.com>
2024-02-13 16:37:06 -0300

    git asesoramiento

---
+++ src/pages/asesoramiento.tsx
@@ -0,0 +1,14 @@
+// mi-nueva-pagina.js
+import React from 'react';
+import CalendlyEmbed from "../components/CalendlyEmbed";
+
+
+const MiNuevaPagina = () => {
+  return (
+    <div>
+      <CalendlyEmbed />
+    </div>
+  );
+};
+
+export default MiNuevaPagina;
