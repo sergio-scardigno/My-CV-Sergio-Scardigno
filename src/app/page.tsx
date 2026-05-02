fc5886df57b25d981dfa68815e2a7c78a2558c17
Sergio <sergioscardigno82@gmail.com>
2026-05-02 08:59:20 -0300

    uupdate2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,7 +106,7 @@ export default function Page() {
             </div>
           </div>
 
-          <Avatar className="h-28 w-28">
+          <Avatar className="h-28 w-28 [&>img]:scale-x-[-1]">
             <AvatarImage alt={RESUME_DATA.name} src={RESUME_DATA.avatarUrl} />
             <AvatarFallback>{RESUME_DATA.initials}</AvatarFallback>
           </Avatar>
